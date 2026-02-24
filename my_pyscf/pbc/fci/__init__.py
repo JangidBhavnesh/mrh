e247bf7e56448a9f4cb4f02b5be19c2db9884c2c
JangidBhavnesh <bhuvnesh.jangid10@gmail.com>
2026-02-24 10:56:08 -0600

    Rename FCI file

--- my_pyscf/pbc/fci/__init__.py
+++ my_pyscf/pbc/fci/__init__.py
@@ -1,9 +1,8 @@
-from mrh.my_pyscf.pbc.fci import direct_com_real
-from mrh.my_pyscf.pbc.fci import rdm_helper
+from mrh.my_pyscf.pbc.fci import direct_spin1_cplx
 from mrh.my_pyscf.pbc.fci import dmrgci_com_real
 
 DMRGCIComplex = dmrgci_com_real.DMRGCIComplex
 
 def solver(cell, singlet, symm=None):
     # Will add the singlet and symm options later.
-    return direct_com_real.FCISolver(cell)
+    return direct_spin1_cplx.FCISolver(cell)
