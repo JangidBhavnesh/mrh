ab9b683b2ec528c73b0f5d5fea360bb1c20e1110
JangidBhavnesh <bhuvnesh.jangid10@gmail.com>
2026-02-24 07:55:13 -0600

    complex DMRGCI interface added

--- my_pyscf/pbc/fci/__init__.py
+++ my_pyscf/pbc/fci/__init__.py
@@ -1,6 +1,8 @@
 from mrh.my_pyscf.pbc.fci import direct_com_real
 from mrh.my_pyscf.pbc.fci import rdm_helper
+from mrh.my_pyscf.pbc.fci import dmrgci_com_real
 
+DMRGCIComplex = dmrgci_com_real.DMRGCIComplex
 
 def solver(cell, singlet, symm=None):
     # Will add the singlet and symm options later.
