dbf9819beb38cc11e95e12258b40724858d23c52
JangidBhavnesh <bhuvnesh.jangid10@gmail.com>
2026-02-26 16:37:31 -0600

    Solve the import error

--- my_pyscf/pbc/fci/__init__.py
+++ my_pyscf/pbc/fci/__init__.py
@@ -1,7 +1,11 @@
 from mrh.my_pyscf.pbc.fci import direct_spin1_cplx
-from mrh.my_pyscf.pbc.fci import dmrgci_cplx_helper
 
-DMRGCIComplex = dmrgci_cplx_helper.DMRGCIComplex
+try:
+    from pyscf import dmrgscf
+    from mrh.my_pyscf.pbc.fci import dmrgci_cplx_helper
+    DMRGCIComplex = dmrgci_cplx_helper.DMRGCIComplex
+except ImportError:
+    pass
 
 def solver(cell, singlet, symm=None):
     # Will add the singlet and symm options later.
