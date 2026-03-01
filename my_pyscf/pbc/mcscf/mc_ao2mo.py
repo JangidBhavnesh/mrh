3c9a2fe8874092d9f118fa28c41fcb0b16f7fa3b
JangidBhavnesh <bhuvnesh.jangid10@gmail.com>
2026-03-01 14:54:37 -0600

    Some more updated in eris ppaa and papa for kCASSCF

--- my_pyscf/pbc/mcscf/mc_ao2mo.py
+++ my_pyscf/pbc/mcscf/mc_ao2mo.py
@@ -7,7 +7,7 @@ from pyscf.pbc.lib import kpts_helper
 # The 2e integrals transformation to MO basis for the orbital optimization.mk
 
 
-def _do_ao2mo_direct(kcasscf, mo_kpts, nkpts, ncore, ncas, nmo):
+def _do_ao2mo_direct(kcasscf, mo_kpts, nkpts, ncore, ncas, nmo, level=1):
     cell = kcasscf._scf.cell
     kpts = kcasscf._scf.kpts
     mydf = kcasscf._scf.with_df
@@ -29,30 +29,41 @@ def _do_ao2mo_direct(kcasscf, mo_kpts, nkpts, ncore, ncas, nmo):
                 papa[k1, k2, k3] = mydf.ao2mo(mo_paaa, kp_tuple, compact=False)
 
     # This is very naive implementation, would require a lot of optimization.
-    j_pc = np.empty((nkpts, nmo, ncore), dtype=dtype)
-    k_pc = np.empty((nkpts, nmo, ncore), dtype=dtype)
-    for k in range(nkpts):
-        mo_ppaa = [mo_kpts[k], mo_kpts[k], mo_kpts[k][:, :ncore], mo_kpts[k][:, :ncore]]
-        temp = mydf.ao2mo(mo_ppaa, [kpts[k]]*4, compact=False)
-        j_pc[k] = np.einsum('ppjj->pj', temp)
-        mo_papa = [mo_kpts[k], mo_kpts[k][:, :ncore], mo_kpts[k][:, ncore:nocc], mo_kpts[k][:, ncore:nocc]]
-        temp = mydf.ao2mo(mo_papa, [kpts[k]]*4, compact=False)
-        k_pc[k] = np.einsum('pjpj->pj', temp)
+    if level == 1:
+        j_pc = np.empty((nkpts, nmo, ncore), dtype=dtype)
+        k_pc = np.empty((nkpts, nmo, ncore), dtype=dtype)
+        for k in range(nkpts):
+            mo_ppaa = [mo_kpts[k], mo_kpts[k], mo_kpts[k][:, :ncore], mo_kpts[k][:, :ncore]]
+            temp = mydf.ao2mo(mo_ppaa, [kpts[k]]*4, compact=False)
+            j_pc[k] = np.einsum('ppjj->pj', temp)
+            mo_papa = [mo_kpts[k], mo_kpts[k][:, :ncore], mo_kpts[k][:, ncore:nocc], mo_kpts[k][:, ncore:nocc]]
+            temp = mydf.ao2mo(mo_papa, [kpts[k]]*4, compact=False)
+            k_pc[k] = np.einsum('pjpj->pj', temp)
+    else:
+        j_pc = None
+        k_pc = None
     return ppaa, papa, j_pc, k_pc
 
 
-def _do_ao2mo_disk(kcasscf, mo_kpts, nkpts, ncore, ncas, nmo):
+def _do_ao2mo_disk(kcasscf, mo_kpts, nkpts, ncore, ncas, nmo, level=1):
     ppaa = None
     papa = None
-    j_pc = None
-    k_pc = None
-    return j_pc, k_pc
+    if level == 1:
+        j_pc = np.empty((nkpts, nmo, ncore), dtype=mo_kpts[0].dtype)
+        k_pc = np.empty((nkpts, nmo, ncore), dtype=mo_kpts[0].dtype)
+        cell = kcasscf._scf.cell
+        kpts = kcasscf._scf.kpts
+    else:
+        j_pc = None
+        k_pc = None
+    erifile = lib.H5TmpFile()
+    return erifile, j_pc, k_pc
 
 def _mem_usage(nkpts, ncore, ncas, nmo):
     basic = nkpts**3 * nmo**2 * ncas**2 * 16 / 1e6
     incore = basic + nkpts**3 * (ncore+ncas) * nmo**3 * 16 / 1e6
     return incore
-
+        
 class _ERIS:
     '''
     AO2MO transformation of the 2e integrals for the orbital optimization step.
@@ -72,6 +83,8 @@ class _ERIS:
 
     def __init__(self, kcasscf, mo_kpts, method='direct', level=1):
         self.erifile = None
+        self.ppaa_kpts = None
+        self.papa_kpts = None
         cell = kcasscf._scf.cell
         kpts = kcasscf._scf.kpts
         ncore = kcasscf.ncore
@@ -98,7 +111,34 @@ class _ERIS:
               mem_incore, mem_now, kcasscf.max_memory)
         if (method == 'direct' and mem_now + mem_incore < 0.9 * kcasscf.max_memory):
             log.debug('Using direct ERI transformation.')
-            self.ppaa, self.papa, self.j_pc, self.k_pc = _do_ao2mo_direct(kcasscf, mo_kpts, nkpts, ncore, ncas, nmo)
+            self.ppaa_kpts, self.papa_kpts, self.j_pc, self.k_pc = _do_ao2mo_direct(kcasscf, mo_kpts, nkpts, ncore, ncas, nmo, level=level)
         else:
             log.debug('Using disk ERI transformation.')
-            self.j_pc, self.k_pc = _do_ao2mo_disk(kcasscf, mo_kpts, nkpts, ncore, ncas, nmo)
+            self.erifile, self.j_pc, self.k_pc = _do_ao2mo_disk(kcasscf, mo_kpts, nkpts, ncore, ncas, nmo, level=level)
+            
+        # To access the ppaa and papa integrals: I am bifurcating the code based on whether 
+        # we are using disk or direct method. If we are using direct method, 
+        # we can directly access the ppaa and papa integrals from the attributes.
+        # If we are using disk method, we need to read the integrals from the disk. 
+        # To avoid writing separate code for accessing the integrals in different methods, 
+        # I am defining two lambda functions that will access the integrals based on the method used.
+        self.ppaa = lambda k1, k2, k3: self.get_ppaa(k1, k2, k3)
+        self.papa = lambda k1, k2, k3: self.get_papa(k1, k2, k3)
+
+    @staticmethod
+    def _kkey(k1, k2, k3):
+        return f"{int(k1)}_{int(k2)}_{int(k3)}"
+
+    def _get(self, eriname, k1, k2, k3):
+        # General wrapper
+        arr = getattr(self, eriname + "_kpts", None)
+        if arr is not None:return arr[k1, k2, k3]
+        assert self.erifile is not None
+        data = self.erifile[f"{eriname}/{self._kkey(k1, k2, k3)}"]
+        return data[()]
+
+    def get_ppaa(self, k1, k2, k3):
+        return self._get("ppaa", k1, k2, k3)
+
+    def get_papa(self, k1, k2, k3):
+        return self._get("papa", k1, k2, k3)
