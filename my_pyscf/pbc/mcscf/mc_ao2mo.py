c887a91ee340b154344bc2dc6cf33be576316e54
JangidBhavnesh <bhuvnesh.jangid10@gmail.com>
2026-03-01 23:09:00 -0600

    AO2MO Transformation for the orbital optimization is matching between two different methods. Weekend paid off...!

--- my_pyscf/pbc/mcscf/mc_ao2mo.py
+++ my_pyscf/pbc/mcscf/mc_ao2mo.py
@@ -1,5 +1,6 @@
 import numpy as np
 from functools import reduce
+import itertools
 from pyscf import lib
 from pyscf.ao2mo import _ao2mo
 from pyscf.pbc.lib import kpts_helper
@@ -12,6 +13,16 @@ logger = lib.logger
 
 # The 2e integrals transformation to MO basis for the orbital optimization.mk
 
+def get_nauxlist(mydf, kpts, nkpts):
+    nauxlist = {}
+    for k1 in range(nkpts):
+        for k2 in range(nkpts):
+            kpti_kptj = np.vstack((kpts[k1], kpts[k2]))
+            assert kpti_kptj.shape == (2, 3)
+            with _load3c(mydf._cderi, mydf._dataname, kpti_kptj) as j3c:
+                nauxlist[(k1,k2)] = j3c.shape[0]
+    return nauxlist
+
 def _do_ao2mo_direct(kcasscf, mo_kpts, nkpts, ncore, ncas, nmo, level=1):
     cell = kcasscf._scf.cell
     kpts = kcasscf._scf.kpts
@@ -19,7 +30,6 @@ def _do_ao2mo_direct(kcasscf, mo_kpts, nkpts, ncore, ncas, nmo, level=1):
     nocc = ncore + ncas
     dtype = mo_kpts[0].dtype
     assert len(mo_kpts) == nkpts
-
     log = lib.logger.Logger(kcasscf.stdout, kcasscf.verbose)
     t1 = t0 = (logger.process_clock(), logger.perf_counter())
 
@@ -57,16 +67,6 @@ def _do_ao2mo_direct(kcasscf, mo_kpts, nkpts, ncore, ncas, nmo, level=1):
     log.timer('density fitting ao2mo j_pc, k_pc', *t2)
     return ppaa, papa, j_pc, k_pc
 
-def get_nauxlist(mydf, kpts, nkpts):
-    nauxlist = {}
-    for k1 in range(nkpts):
-        for k2 in range(nkpts):
-            kpti_kptj = np.vstack((kpts[k1], kpts[k2]))
-            assert kpti_kptj.shape == (2, 3)
-            with _load3c(mydf._cderi, mydf._dataname, kpti_kptj) as j3c:
-                nauxlist[(k1,k2)] = j3c.shape[0]
-    return nauxlist
-
 def _do_ao2mo_disk(kcasscf, mo_kpts, nkpts, ncore, ncas, nmo, level=1):
     cell = kcasscf._scf.cell
     kpts = kcasscf._scf.kpts
@@ -77,47 +77,58 @@ def _do_ao2mo_disk(kcasscf, mo_kpts, nkpts, ncore, ncas, nmo, level=1):
 
     assert len(mo_kpts) == nkpts
 
+    # Note: There are two h5py files created, one temp file which hold intermediates for the ppaa and papa integrals, 
+    # and another which will store those integrals. The first file will be deleted after these integrals
+    # are constructed, and only the second file will be used for accessing the integrals.
     erifile = lib.H5TmpFile()
-    erifile.require_group("ppaa") # Create the group for storing the ppaa integrals.
-    erifile.require_group("papa") # Create the group for storing the papa integrals.
+    erifile.require_group("ppaa")
+    erifile.require_group("papa")
 
+    t1 = t0 = (logger.process_clock(), logger.perf_counter())
     log = lib.logger.Logger(kcasscf.stdout, kcasscf.verbose)
-    # Steps:
-    # 1. Loop over k1, k2 pairs and compute the ao2mo for these pairs, and save them on disk.
     
-
     mem_now = lib.current_memory()[0]
-    # I am not sure wheather the naoaux will be same for all k1, k2 pairs. 
-    # So I am taking the maximum naoaux among all pairs.
+    
+    # I am not sure wheather the naoaux will be same for all k1, k2 pairs. So I am taking the maximum naoaux among all pairs.
     # naoaux = mydf.get_naoaux()
     nauxlist = get_nauxlist(mydf, kpts, nkpts)
     naoaux = max(nauxlist.values())
+
+    # Note the datatype is complex double, so 16 bytes.
     mem_required = naoaux * nmo * nmo * 16 / 1e6
     if mem_now < 2.0 * mem_required:
         raise MemoryError(f"Not enough memory for intermediate arrays for ao2mo transformation. \
                           Required: {mem_required} MB, Current: {mem_now} MB.")
     
-    compact = False # For complex integrals: I am using compact as of now to avoid any conj/sign issues.
-    fxpp = lib.H5TmpFile()
-    grp = fxpp.require_group("xpp") # Create the group for storing the Lpq integrals.
-    grp2 = fxpp.require_group("xpp_sign") # Create the group for storing sign of the Lpq integrals.
-    
-    # Step-1
-    for k1 in range(nkpts):
-        for k2 in range(nkpts):
-            mo_coeffs = _mo_as_complex([mo_kpts[k1], mo_kpts[k2]])
-            nij_pair, moij, ijslice = _conc_mos(mo_coeffs[0], mo_coeffs[1])[1:]
-            kptij = np.vstack((kpts[k1], kpts[k2]))
-            naux = nauxlist[(k1, k2)]
-            zij = None
-            for LpqR, LpqI, sign in mydf.sr_loop(kptij, mem_now, compact):
-                tao = []
-                ao_loc = None
-                zij = _ao2mo.r_e2(LpqR + 1j * LpqI, moij, ijslice, tao, ao_loc, out=zij)
-                # TODO: Learn whether I should store the 2D or 3D array for better I/O performance.
-                grp.create_dataset(f"{k1}_{k2}", data=zij.reshape(naux, nmo, nmo))
-                grp2.create_dataset(f"{k1}_{k2}", data=sign)
+    # For complex integrals: I am using compact=False as of now to avoid any conj/sign issues.
+    # However, I haven't encountered the code in PySCF which uses the compact=True option for complex integrals.
+    # TODO: Explore the compact option for the complex integrals.
+    compact = False 
 
+    # Intermediate temp file
+    fxpp = lib.H5TmpFile()
+    grp = fxpp.require_group("xpp")
+    grp2 = fxpp.require_group("xpp_sign")
+    
+    # Step-1: Compute the Lpq integrals and save them on disk. 
+    # We will read them later to construct the ppaa and papa integrals.
+    # for k1 in range(nkpts):
+    #     for k2 in range(nkpts):
+    for k1, k2 in itertools.product(range(nkpts), repeat=2):
+        mo_coeffs_tmp = _mo_as_complex([mo_kpts[k1], mo_kpts[k2]])
+        moij, ijslice = _conc_mos(mo_coeffs_tmp[0], mo_coeffs_tmp[1])[2:]
+        kptij = np.vstack((kpts[k1], kpts[k2]))
+        naux = nauxlist[(k1, k2)]
+        zij = None
+        for LpqR, LpqI, sign in mydf.sr_loop(kptij, mem_now, compact):
+            tao = []
+            ao_loc = None
+            zij = _ao2mo.r_e2(LpqR + 1j * LpqI, moij, ijslice, tao, ao_loc, out=zij)
+            # TODO: Learn whether I should store the 2D or 3D array for better I/O performance.
+            grp.create_dataset(f"{k1}_{k2}", data=zij.reshape(naux, nmo, nmo))
+            grp2.create_dataset(f"{k1}_{k2}", data=sign)
+
+    t1 = log.timer('density fitting ao2mo Lpq', *t0)
     # TODO: use the blksize to loop over the nmo to reduce the memory footprint.           
     # Step-2: Construct the papa integrals:
     kconserv = kpts_helper.get_kconserv(cell, kpts)
@@ -127,45 +138,47 @@ def _do_ao2mo_disk(kcasscf, mo_kpts, nkpts, ncore, ncas, nmo, level=1):
     for k1, k2, k3 in kpts_helper.loop_kkk(nkpts):
         k4 = kconserv[k1, k2, k3]
         papa = np.zeros((nmo*ncas, nmo*ncas), dtype=dtype)
-        # Read the Lpq integrals for the required pairs from disk.
-        zij_12 = grp[f"{k1}_{k2}"][:, :, :ncas][()]
-        zkl_34 = grp[f"{k3}_{k4}"][:, :, :ncas][()]
-        # Reshape 
+        zij_12 = grp[f"{k1}_{k2}"][:, :, ncore:ncore+ncas][()]
+        zkl_34 = grp[f"{k3}_{k4}"][:, :, ncore:ncore+ncas][()]
         zij_12 = zij_12.reshape(-1, nmo*ncas)
         zkl_34 = zkl_34.reshape(-1, nmo*ncas)
         sign = grp2[f"{k1}_{k2}"][()]
         lib.dot(zij_12.T, zkl_34, sign, papa, 1)
         erifile[f"papa/{k1}_{k2}_{k3}"] = papa.reshape(nmo, ncas, nmo, ncas)
 
-    papa = None
-    
+    papa = zij_12 = zkl_34 = None
+    t2 = log.timer('density fitting ao2mo papa', *t1)
+
     # Step-3: Construct the ppaa integrals:
     for k1, k2, k3 in kpts_helper.loop_kkk(nkpts):
         k4 = kconserv[k1, k2, k3]
         ppaa = np.zeros((nmo*nmo, ncas*ncas), dtype=dtype)
         zij_12 = grp[f"{k1}_{k2}"][()]
-        zkl_34 = grp[f"{k3}_{k4}"][:, :ncas, :ncas][()] # Lkl for k3=k4
+        zkl_34 = grp[f"{k3}_{k4}"][:, ncore:ncore+ncas, ncore:ncore+ncas][()]
         zij_12 = zij_12.reshape(-1, nmo*nmo)
         zkl_34 = zkl_34.reshape(-1, ncas*ncas)
         sign = grp2[f"{k1}_{k2}"][()]
         lib.dot(zij_12.T, zkl_34, sign, ppaa, 1)
         erifile[f"ppaa/{k1}_{k2}_{k3}"] = ppaa.reshape(nmo, nmo, ncas, ncas)
 
-    ppaa = None
+    ppaa = zij_12 = zkl_34 = None
+    t2 = log.timer('density fitting ao2mo ppaa', *t2)
 
     if level == 1:
-        j_pc_kpts = np.empty((nkpts, nmo, ncore), dtype=dtype)
-        k_pc_kpts = np.empty((nkpts, nmo, ncore), dtype=dtype)
+        j_pc_kpts = np.zeros((nkpts, nmo, ncore), dtype=dtype)
+        k_pc_kpts = np.zeros((nkpts, nmo, ncore), dtype=dtype)
         for k in range(nkpts):
-            zij = grp[f"{k}_{k}"][:]
+            zij = grp[f"{k}_{k}"][()]
             bufd = np.einsum('pii->pi', zij)
             j_pc_kpts[k] = np.einsum('pi,pj->ij', bufd, bufd[:,:ncore])
             k_cp = np.einsum('kij,kij->ij', zij[:,:ncore], zij[:,:ncore])
             k_pc_kpts[k] = k_cp.conj().T
+        
+        bufd = zij =  k_cp = None
     else:
         j_pc_kpts = None
         k_pc_kpts = None
-    
+    log.timer('density fitting ao2mo j_pc and k_pc', *t2)
     fxpp.close()
     return erifile, j_pc_kpts, k_pc_kpts
 
@@ -189,24 +202,24 @@ class _ERIS:
         level: int (Default is 1)
             level-1: ppaa, papa, vhf, jpc and kpc
             level-2: Only ppaa, papa and vhf
-    Saved Attributes:
-        ppaa: function
-            A function that takes k1, k2, k3 as input and returns the ppaa integrals for these k-points.
-        papa: function
-            A function that takes k1, k2, k3 as input and returns the papa integrals for these k-points.
-        j_pc: numpy array or None
-            The J contribution to the core Hamiltonian in MO basis. It is required for the orbital optimization step. It is only computed if level=1.
-        k_pc: numpy array or None
-            The K contribution to the core Hamiltonian in MO basis. It is required for the orbital optimization step. It is only computed if level=1.
-        vhf_c: numpy array
-            The VHF contribution to the core Hamiltonian in MO basis. It is required for the orbital optimization step.
-    '''
 
+    Saved Attributes:
+        ppaa: np.array (nkpts, nkpts, nkpts, nmo, nmo, ncas, ncas)/ read from disk 
+            It's a function that takes k1, k2, k3 as input and returns the ppaa integrals
+        papa: np.array (nkpts, nkpts, nkpts, nmo, ncas, nmo, ncas)/ read from disk
+            It's a function that takes k1, k2, k3 as input and returns the papa integrals
+        j_pc: np.array (nkpts, nmo, ncore)
+            potential due to core electrons
+        k_pc: np.array (nkpts, nmo, ncore)
+            potential due to core electrons
+        vhf_c: np.array (nkpts, nmo, nmo)
+            VHF matrix due to core electrons
+    '''
     def __init__(self, kcasscf, mo_kpts, method='direct', level=1):
         self.erifile = None
         self.ppaa_kpts = None
         self.papa_kpts = None
-
+        log = lib.logger.Logger(kcasscf.stdout, kcasscf.verbose)
         cell = kcasscf._scf.cell
         kpts = kcasscf._scf.kpts
         ncore = kcasscf.ncore
@@ -215,6 +228,7 @@ class _ERIS:
         nao, nmo = mo_kpts[0].shape
         dtype = mo_kpts[0].dtype
 
+        t1 = t0 = (logger.process_clock(), logger.perf_counter())
         dmcore_kpts = np.asarray(
             [2.0 * (mo_kpts[k][:, :ncore] @ mo_kpts[k][:, :ncore].conj().T) 
              for k in range(nkpts)], 
@@ -225,19 +239,21 @@ class _ERIS:
             [reduce(np.dot, (mo_kpts[k].conj().T, 2.0 * vj_kpts[k] - vk_kpts[k], mo_kpts[k]))
              for k in range(nkpts)], 
              dtype=dtype)
+        t1 = log.timer('vhf construction for core density', *t1)
 
         mem_incore = _mem_usage(nkpts, ncore, ncas, nmo)
         mem_now = lib.current_memory()[0]
-        log = lib.logger.Logger(kcasscf.stdout, kcasscf.verbose)
+        
         log.debug('Memory usage for incore ERI transformation: %.2f MB.Current memory usage: %.2f MB. Max memory: %.2f MB.',
               mem_incore, mem_now, kcasscf.max_memory)
         if (method == 'direct' and mem_now + mem_incore < 0.9 * kcasscf.max_memory):
             log.debug('Using direct ERI transformation.')
             self.ppaa_kpts, self.papa_kpts, self.j_pc, self.k_pc = _do_ao2mo_direct(kcasscf, mo_kpts, nkpts, ncore, ncas, nmo, level=level)
+            t1 = log.timer('direct ao2mo', *t1)
         else:
             log.debug('Using disk ERI transformation.')
             self.erifile, self.j_pc, self.k_pc = _do_ao2mo_disk(kcasscf, mo_kpts, nkpts, ncore, ncas, nmo, level=level)
-            
+            t1 = log.timer('disk ao2mo', *t1)
         # To access the ppaa and papa integrals: I am bifurcating the code based on whether 
         # we are using disk or direct method. If we are using direct method, 
         # we can directly access the ppaa and papa integrals from the attributes.
@@ -246,6 +262,8 @@ class _ERIS:
         # I am defining two lambda functions that will access the integrals based on the method used.
         self.ppaa = lambda k1, k2, k3: self.get_ppaa(k1, k2, k3)
         self.papa = lambda k1, k2, k3: self.get_papa(k1, k2, k3)
+
+        log.timer('Total ERI transformation', *t0)
 
     @staticmethod
     def _kkey(k1, k2, k3):
@@ -264,7 +282,6 @@ class _ERIS:
 
     def get_papa(self, k1, k2, k3):
         return self._get("papa", k1, k2, k3)
-    
 
 
 if __name__ == "__main__":
@@ -275,24 +292,26 @@ if __name__ == "__main__":
     cell = gto.Cell()
     cell.a = np.eye(3)*3.5668
     cell.atom = 'C 0 0 0'
-    cell.basis = 'CC-PVQZ'
+    cell.basis = 'CC-PVDZ'
     cell.verbose = lib.logger.TIMER_LEVEL
     cell.build()
 
-    kmesh = [1, 1, 2]
+    kmesh = [2, 2, 2]
     kpts = cell.make_kpts(kmesh)
     
     kmf = scf.KRHF(cell, kpts).density_fit()
     kmf.exxdiv = None
+    kmf.max_cycle = 100
+    kmf.conv_tol = 1e-12
     kmf.kernel()
     
-    # Dummy class to check the _ERIS class.
     class _kCASSCF:
+        # Dummy class to check the _ERIS class.
         def __init__(self, kmf, ncas, nelecas):
             self._scf = kmf
             self.ncas = ncas
             self.nelecas = nelecas
-            self.ncore = int(kmf.cell.nelectron - nelecas)
+            self.ncore = int((kmf.cell.nelectron - nelecas) / 2)
             self.nkpts = len(kmf.kpts)
             self.stdout = kmf.stdout
             self.verbose = kmf.verbose
@@ -301,7 +320,33 @@ if __name__ == "__main__":
     kmc = _kCASSCF(kmf, ncas=2, nelecas=2)
     mo_kpts = kmf.mo_coeff
 
-    eris = _ERIS(kmc, mo_kpts, method='disk', level=1)
+    nmo = mo_kpts[0].shape[1]
+    ncas = kmc.ncas
+    ncore = kmc.ncore
+    nkpts = kmc.nkpts
 
+    eris = _ERIS(kmc, mo_kpts, method='disk', level=1)
     eris2 = _ERIS(kmc, mo_kpts, method='direct', level=1)
+
+    def compare_integrals(arr1, arr2, name, shape):
+        assert arr1.dtype == arr2.dtype
+        assert arr1.shape == arr2.shape == shape
+        assert np.allclose(arr1, arr2), f"{name} integrals do not match between direct and disk method."
+        print(f"{name} integrals match between direct and disk method.")
+        arr1 = arr2 = None
+
+    # Compare the j_pc and k_pc integrals
+    compare_integrals(eris.j_pc, eris2.j_pc, "j_pc", (nkpts, nmo, ncore))
+    compare_integrals(eris.k_pc, eris2.k_pc, "k_pc", (nkpts, nmo, ncore))
     
+    # Compare the vhf_c integrals
+    compare_integrals(eris.vhf_c, eris2.vhf_c, "vhf_c", (nkpts, nmo, nmo))
+
+    # Compare the ppaa and papa integrals
+    # Gamma point (k1=k2=k3=0)
+    compare_integrals(eris.ppaa(0, 0, 0), eris2.ppaa(0, 0, 0), "ppaa", (nmo, nmo, ncas, ncas))
+    compare_integrals(eris.papa(0, 0, 0), eris2.papa(0, 0, 0), "papa", (nmo, ncas, nmo, ncas))
+    
+    # Non-Gamma point (k1=0, k2=0, k3=1)
+    compare_integrals(eris.ppaa(0, 0, 1), eris2.ppaa(0, 0, 1), "ppaa", (nmo, nmo, ncas, ncas))
+    compare_integrals(eris.papa(0, 0, 1), eris2.papa(0, 0, 1), "papa", (nmo, ncas, nmo, ncas))
