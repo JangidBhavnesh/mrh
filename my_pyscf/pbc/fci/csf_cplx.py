a3808b7ee35784e185de19236857bdde0530c37f
JangidBhavnesh <bhuvnesh.jangid10@gmail.com>
2026-03-06 08:47:13 -0600

    More updates on cplx CSF solver. 1. Fixed some import bugs. 2. Completed the contract_2e part. 3. Added some commentary for my understanding.

--- my_pyscf/pbc/fci/csf_cplx.py
+++ my_pyscf/pbc/fci/csf_cplx.py
@@ -2,11 +2,25 @@ import numpy as np
 
 from pyscf import lib
 from pyscf import __config__
+from pyscf.fci import direct_uhf
 from pyscf.csf_fci.csf import CSFFCISolver as realCSFFCISolver
+from pyscf.csf_fci.csf import unpack_h1e_cs
+from pyscf.lib.numpy_helper import tag_array
 
 from mrh.my_pyscf.pbc.fci import direct_spin1_cplx
 
+'''
 # Okay Great. Let me implement the CSFsolver with complex Hamiltonian.
+# Logic of CSFSolvers in PySCF:
+#   1. Init guess in det basis, then transform to CSF basis.
+#   2. Contruct the Hamiltonian in det basis (using PySCF infrastructure), then transform to CSF basis.
+#   3. Solve the eigenvalue problem (exact or Davidson) in CSF basis.
+#   4. Transform the eigenvectors back to det basis.
+
+# Now for the complex CI vec of type (a+ib), to use the CSFSolver, the transformation
+# will be a_csf + i*b_csf = U * (a_det + i*b_det), where U is the det to CSF transformation matrix (not this is
+# matrix is not constructed and stored in the memory). The U matrix is real only.
+'''
 
 def pspace(**args):
     pass
@@ -22,11 +36,15 @@ def make_hdiag_det (h1e, eri, nrob, nelec):
     # However that won't be directly applicable as the h1e and eri are complex.
     pass
 
-class CSFTransformer:
-    pass
 
 class cplxCSFFCISolver:
+    '''
+    Parent class for the complex FCI solver in CSF basis. This class will implement the 
+    necessary functions. 
+    # Borrowing functions from the real CSF solver. Only modifying the functions
+    # which are directly needed.
     # This class won't be of any use for standalone.
+    '''
     _keys = {'smult', 'trasnsformers'}
     pspace_size = getattr(__config__, 'fci_csf_FCI_pspace_size', 200)
     make_hdiag = make_hdiag_det
@@ -36,19 +54,38 @@ class cplxCSFFCISolver:
         self.transformers = None
         super().__init__ (**args)
 
-    # Borrowing functions from the real CSF solver. Only modifying the functions
-    # which are directly needed.
-    make_hdiag_csf = lib.module_method(realCSFFCISolver.make_hdiag_csf)
-    absorb_h1e = lib.module_method(realCSFFCISolver.absorb_h1e)
+    def make_hdiag_csf(self, h1e, eri, norb, nelec, hdiag_det=None, smult=None, max_memory=None):
+        self.norb = norb
+        self.nelec = nelec
+        if smult is not None:
+            self.smult = smult
+        self.check_transformer_cache ()
+        max_memory = max_memory if max_memory is not None else self.max_memory
+
+        return self.make_hdiag_csf(h1e, eri, norb, nelec, self.transformer, hdiag_det=hdiag_det, 
+                                   max_memory=max_memory)
+
+    def absorb_h1e(self, h1e, eri, norb, nelec, fac=1):
+        h1e_c, h1e_s = unpack_h1e_cs(h1e)
+        h2eff = super().absorb_h1e(h1e_c, eri, norb, nelec, fac=fac)
+        if h1e_s is not None:
+            h2eff = tag_array(h2eff, h1e_s=h1e_s)
+        return h2eff
+    
     log_transformer_cache = lib.module_method(realCSFFCISolver.log_transformer_cache)
     print_transformer_cache = lib.module_method(realCSFFCISolver.print_transformer_cache)
 
     def contract_2e(self, eris, fcivec, norb, nelec, link_index=None, **kwargs):
         hc = super().contract_2e(eris, fcivec, norb, nelec, link_index=link_index, **kwargs)
         if hasattr(eris, 'h1e_s'):
-            pass
-            # Need to implement this function.
-            #hc += direct_uhf.contract_1e ([eri.h1e_s, -eri.h1e_s], fcivec, norb, nelec, link_index)
+            hc_real = direct_uhf.contract_1e ([eris.h1e_s.real, -eris.h1e_s.real], fcivec.real, norb, nelec, link_index)
+            hc_real -= direct_uhf.contract_1e ([eris.h1e_s.imag, -eris.h1e_s.imag], fcivec.imag, norb, nelec, link_index)
+            hc.real += hc_real
+            hc_real = None
+            hc_imag = direct_uhf.contract_1e ([eris.h1e_s.real, -eris.h1e_s.real], fcivec.imag, norb, nelec, link_index)
+            hc_imag += direct_uhf.contract_1e ([eris.h1e_s.imag, -eris.h1e_s.imag], fcivec.real, norb, nelec, link_index)
+            hc.imag += hc_imag
+            hc_imag = None
         return hc
     
     def pspace (self, h1e, eri, norb, nelec, hdiag_det=None, hdiag_csf=None, npsp=200, **kwargs):
@@ -60,10 +97,16 @@ class cplxCSFFCISolver:
         return pspace (self, h1e, eri, norb, nelec, self.transformer, hdiag_det=hdiag_det,
                        hdiag_csf=hdiag_csf, npsp=npsp, max_memory=max_memory)
 
-
-# Good chance to learn Inheritance, and MRI Method:
+# Good chance to learn Inheritance, and MRO Method:
 class FCISolver(cplxCSFFCISolver, direct_spin1_cplx.FCISolver):
+    '''
+    Complex FCI in CSFSolver. 
+    '''
+
     def get_init_guess(self, norb, nelec, nroots, hdiag_csf, **kwargs):
+        '''
+        Get the initial guess for the FCI calculation in the CSF basis.
+        '''
         self.norb = norb
         self.nelec = nelec
         self.check_transformer_cache ()
@@ -75,16 +118,13 @@ class FCISolver(cplxCSFFCISolver, direct_spin1_cplx.FCISolver):
         self.smult = kwargs.pop('smult', self.smult)
         self.check_transformer_cache ()
         self.log_transformer_cache (lib.logger.DEBUG)
+
         e, c = kernel (self, h1e, eri, norb, nelec, smult=self.smult,
-            idx_sym=None, ci0=ci0, transformer=self.transformer, **kwargs)
+                       idx_sym=None, ci0=ci0, transformer=self.transformer,
+                       **kwargs)
+
         self.eci, self.ci = e, c
         return e, c
 
-    def check_transformer_cache (self):
-        assert (isinstance (self.smult, (int, np.number)))
-        neleca, nelecb = direct_spin1_cplx._unpack_nelec (self.nelec)
-        if self.transformer is None:
-            self.transformer = CSFTransformer (self.norb, neleca, nelecb, self.smult,
-                                               max_memory=self.max_memory)
-        else:
-            self.transformer._update_spin_cache (self.norb, neleca, nelecb, self.smult)
+    check_transformer_cache = lib.module_method(realCSFFCISolver.check_transformer_cache)
+    
