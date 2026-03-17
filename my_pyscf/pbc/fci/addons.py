5a647861b79555e3fef0966784390ccac1d9c665
JangidBhavnesh <bhuvnesh.jangid10@gmail.com>
2026-03-17 10:32:19 -0500

    Some wrapper functions

--- my_pyscf/pbc/fci/addons.py
+++ my_pyscf/pbc/fci/addons.py
@@ -1,8 +1,8 @@
-from pyscf.fci import addons
+from pyscf.fci import addons, cistring
 
 # Calling the spin-penalty function in addons file to keep it consistent with PySCF.
 # I have added the contract_ss, so I hope I don't need to write the code for these
-# function, rather these should work.
+# function, rather this should work.
 
 # Should I rename the class calling !? Probably no!.
 
@@ -10,3 +10,15 @@ fix_spin = addons.fix_spin
 fix_spin_ = addons.fix_spin_
 SpinPenaltyFCISolver = addons.SpinPenaltyFCISolver
 
+# Helper function to unpack electrons and active space.
+def _unpack_nelec(nelec, spin=None):
+    return addons._unpack_nelec(nelec, spin)
+
+def _unpack(norb, nelec, link_index, spin=None):
+    if link_index is None:
+        neleca, nelecb = _unpack_nelec(nelec, spin)
+        link_indexa = cistring.gen_linkstr_index(range(norb), neleca)
+        link_indexb = cistring.gen_linkstr_index(range(norb), nelecb)
+        return link_indexa, link_indexb
+    else:
+        return link_index
