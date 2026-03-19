32f99d5acdafe7217e4a4bef8c229201f57cfb0a
JangidBhavnesh <bhuvnesh.jangid10@gmail.com>
2026-03-19 12:26:21 -0500

    Let's start using k-CASSCF

--- my_pyscf/pbc/mcscf/__init__.py
+++ my_pyscf/pbc/mcscf/__init__.py
@@ -1,9 +1,14 @@
 
 from pyscf.pbc import scf
 from mrh.my_pyscf.pbc.mcscf import casci
-
+from mrh.my_pyscf.pbc.mcscf import mc1step
 
 def CASCI(kmf, ncas, nelecas, ncore=None):
     assert isinstance(kmf, scf.hf.SCF),  "CASCI only works with periodic SCF objects"
     kmc = casci.CASCI(kmf, ncas, nelecas, ncore)
+    return kmc
+
+def CASSCF(kmf, ncas, nelecas, ncore=None):
+    assert isinstance(kmf, scf.hf.SCF),  "CASSCF only works with periodic SCF objects"
+    kmc = mc1step.CASSCF(kmf, ncas, nelecas, ncore)
     return kmc
