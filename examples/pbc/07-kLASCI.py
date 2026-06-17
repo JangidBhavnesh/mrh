e73fcea0347571a53fb2100f7078b7f1b6af21d3
JangidBhavnesh <bhuvnesh.jangid10@gmail.com>
2026-06-17 08:23:52 -0500

    Initial framework example/test file

--- examples/pbc/07-kLASCI.py
+++ examples/pbc/07-kLASCI.py
@@ -5,12 +5,9 @@ from pyscf import lib
 from pyscf.pbc import scf
 from pyscf.pbc import gto as pgto
 
-from mrh.my_pyscf.pbc.fci import csf_solver
 from mrh.my_pyscf.pbc import mcscf
 from mrh.my_pyscf.pbc.mcscf import avas
-from mrh.my_pyscf.pbc.mcscf import KLASCI
-from mrh.my_pyscf.pbc.util.pbcmolden import print_molden_only_as, print_molden_wannier, print_molden_natorbs
-from mrh.my_pyscf.pbc.mcscf.k2R import  get_mo_coeff_k2R
+from mrh.my_pyscf.pbc.util.transym import localize_mo_coeff
 
 np.set_printoptions(precision=4, suppress=True)
 
@@ -23,7 +20,7 @@ cell.atom ='''
 H 0.0 0.0 0.0
 H 0.74 0.0 0.0
 '''
-cell.basis = '6-31G'
+cell.basis = 'CC-PVDZ'
 cell.unit = 'Angstrom'
 cell.max_memory = 120000
 cell.ke_cutoff = 100
@@ -31,49 +28,23 @@ cell.precision = 1e-10
 cell.verbose = lib.logger.INFO
 cell.build()
 
-kmesh = [6, 6, 6]
+# Define the k-point mesh and the k-points for the calculation.
+kmesh = [3, 1, 1]
 kpts = cell.make_kpts(kmesh, wrap_around=True)
 
+# Mean-field calculation.
 kmf = scf.KRHF(cell, kpts=kpts).density_fit(auxbasis='def2-svp-jkfit')
 kmf.max_cycle=1000
 kmf.exxdiv = None
 kmf.conv_tol = 1e-10
 kmf.kernel()
 
+# Active space selection.
 mo_coeff = avas.kernel(kmf, ['H 1s'], minao=cell.basis)[2]
 
-from mrh.my_pyscf.pbc.util.transym import TranslationSymm, localize_kmf_mo_coeff, check_wannier_translation, get_wannier_orbs,pack_wannier_orb, unpack_wannier_orb, make_wannier_matrix, make_ovlp_mat_in_wannier_basis, orthogonality_check
-ts = TranslationSymm(cell, kmesh)
+# Now we can run the k-LASCI calculation.
+klas = mcscf.KLASCI(kmf, 2, (1, 1), kmesh=kmesh)
+# Localization of the active space orbitals, this is important.
+lo_coeff = localize_mo_coeff(klas, mo_coeff, ['H 1s'])[0]
 
-lo_coeff = localize_kmf_mo_coeff(kmf, mo_coeff)[0]
-
-wannier_orb, R_indices = get_wannier_orbs(kmf, kmesh, lo_coeff)
-check_wannier_translation(ts, wannier_orb, R_indices)
-
-# # Orthogonality check for the Wannier orbitals in packed form.
-# wannier_orb = make_wannier_matrix(wannier_orb)
-# ovlp_k = kmf.get_ovlp(kpts=kpts)
-# ovlp_bvk = make_ovlp_mat_in_wannier_basis(kmf, kmesh)
-# orthogonality_check(wannier_orb, ovlp_bvk)
-# print('Orthogonality check passed! for wannier orbitals')
-
-wannier_packed = pack_wannier_orb(wannier_orb, ref_cell=0)
-wannier_unpacked = unpack_wannier_orb(wannier_packed, cell, kmesh, ref_cell=0)
-assert np.allclose(wannier_orb, wannier_unpacked), "Wannier unpacking failed, something went wrong in the packing/unpacking functions."
-print("Wannier packing/unpacking check passed!")
-
-print_molden_wannier(wannier_packed, cell, kmesh, 'wannier_orb.molden', occ=None)
-exit()
-
-# Print the molden file for the active space orbitals.
-print_molden_only_as(kmf, mo_coeff, kmesh, 'mo_coeff.molden', 2, 0)
-
-
-# kmc = mcscf.CASSCF(kmf, 2, 2)
-# kmc.kpts = kpts
-# kmc.kmesh = kmesh
-# kmc.fcisolver = csf_solver(cell, smult=1)
-# kmc.max_cycle_macro = 50
-# kmc.kernel(mo_coeff)
-
-# print_molden_natorbs(kmc, kmesh, 'natorbs.molden')
+# klas.kernel(lo_coeff)
