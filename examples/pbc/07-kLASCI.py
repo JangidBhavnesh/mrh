6f0b4a6ed783a01b40a286e3243d31f0e94243ea
JangidBhavnesh <bhuvnesh.jangid10@gmail.com>
2026-06-16 22:53:02 -0500

    Intermediate commit,

--- examples/pbc/07-kLASCI.py
+++ examples/pbc/07-kLASCI.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy
 
 from pyscf import lib
 from pyscf.pbc import scf
@@ -8,7 +9,10 @@ from mrh.my_pyscf.pbc.fci import csf_solver
 from mrh.my_pyscf.pbc import mcscf
 from mrh.my_pyscf.pbc.mcscf import avas
 from mrh.my_pyscf.pbc.mcscf import KLASCI
-from mrh.my_pyscf.pbc.util.pbcmolden import print_molden_only_as, print_molden_natorbs
+from mrh.my_pyscf.pbc.util.pbcmolden import print_molden_only_as, print_molden_wannier, print_molden_natorbs
+from mrh.my_pyscf.pbc.mcscf.k2R import  get_mo_coeff_k2R
+
+np.set_printoptions(precision=4, suppress=True)
 
 # Example file to use the kLASCI.
 # Author: Bhavnesh Jangid
@@ -27,7 +31,7 @@ cell.precision = 1e-10
 cell.verbose = lib.logger.INFO
 cell.build()
 
-kmesh = [3, 1, 1]
+kmesh = [6, 6, 6]
 kpts = cell.make_kpts(kmesh, wrap_around=True)
 
 kmf = scf.KRHF(cell, kpts=kpts).density_fit(auxbasis='def2-svp-jkfit')
@@ -37,14 +41,39 @@ kmf.conv_tol = 1e-10
 kmf.kernel()
 
 mo_coeff = avas.kernel(kmf, ['H 1s'], minao=cell.basis)[2]
+
+from mrh.my_pyscf.pbc.util.transym import TranslationSymm, localize_kmf_mo_coeff, check_wannier_translation, get_wannier_orbs,pack_wannier_orb, unpack_wannier_orb, make_wannier_matrix, make_ovlp_mat_in_wannier_basis, orthogonality_check
+ts = TranslationSymm(cell, kmesh)
+
+lo_coeff = localize_kmf_mo_coeff(kmf, mo_coeff)[0]
+
+wannier_orb, R_indices = get_wannier_orbs(kmf, kmesh, lo_coeff)
+check_wannier_translation(ts, wannier_orb, R_indices)
+
+# # Orthogonality check for the Wannier orbitals in packed form.
+# wannier_orb = make_wannier_matrix(wannier_orb)
+# ovlp_k = kmf.get_ovlp(kpts=kpts)
+# ovlp_bvk = make_ovlp_mat_in_wannier_basis(kmf, kmesh)
+# orthogonality_check(wannier_orb, ovlp_bvk)
+# print('Orthogonality check passed! for wannier orbitals')
+
+wannier_packed = pack_wannier_orb(wannier_orb, ref_cell=0)
+wannier_unpacked = unpack_wannier_orb(wannier_packed, cell, kmesh, ref_cell=0)
+assert np.allclose(wannier_orb, wannier_unpacked), "Wannier unpacking failed, something went wrong in the packing/unpacking functions."
+print("Wannier packing/unpacking check passed!")
+
+print_molden_wannier(wannier_packed, cell, kmesh, 'wannier_orb.molden', occ=None)
+exit()
+
 # Print the molden file for the active space orbitals.
 print_molden_only_as(kmf, mo_coeff, kmesh, 'mo_coeff.molden', 2, 0)
 
-kmc = mcscf.CASSCF(kmf, 2, 2)
-kmc.kpts = kpts
-kmc.kmesh = kmesh
-kmc.fcisolver = csf_solver(cell, smult=1)
-kmc.max_cycle_macro = 50
-kmc.kernel(mo_coeff)
 
-print_molden_natorbs(kmc, kmesh, 'natorbs.molden')
+# kmc = mcscf.CASSCF(kmf, 2, 2)
+# kmc.kpts = kpts
+# kmc.kmesh = kmesh
+# kmc.fcisolver = csf_solver(cell, smult=1)
+# kmc.max_cycle_macro = 50
+# kmc.kernel(mo_coeff)
+
+# print_molden_natorbs(kmc, kmesh, 'natorbs.molden')
