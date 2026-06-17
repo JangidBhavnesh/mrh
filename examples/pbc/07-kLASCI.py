a746e8c55354da196336ef185701adf05abc181a
JangidBhavnesh <bhuvnesh.jangid10@gmail.com>
2026-06-17 16:58:56 -0500

    Intermediate commit

--- examples/pbc/07-kLASCI.py
+++ examples/pbc/07-kLASCI.py
@@ -20,7 +20,7 @@ cell.atom ='''
 H 0.0 0.0 0.0
 H 0.74 0.0 0.0
 '''
-cell.basis = 'CC-PVDZ'
+cell.basis = '631G'
 cell.unit = 'Angstrom'
 cell.max_memory = 120000
 cell.ke_cutoff = 100
@@ -35,6 +35,8 @@ kpts = cell.make_kpts(kmesh, wrap_around=True)
 # Mean-field calculation.
 kmf = scf.KRHF(cell, kpts=kpts).density_fit(auxbasis='def2-svp-jkfit')
 kmf.max_cycle=1000
+# kmf.with_df._cderi_to_save = "cderi.h5"
+kmf.with_df._cderi = 'cderi.h5'
 kmf.exxdiv = None
 kmf.conv_tol = 1e-10
 kmf.kernel()
@@ -46,5 +48,4 @@ mo_coeff = avas.kernel(kmf, ['H 1s'], minao=cell.basis)[2]
 klas = mcscf.KLASCI(kmf, 2, (1, 1), kmesh=kmesh)
 # Localization of the active space orbitals, this is important.
 lo_coeff = localize_mo_coeff(klas, mo_coeff, ['H 1s'])[0]
-
-# klas.kernel(lo_coeff)
+klas.kernel(lo_coeff)
