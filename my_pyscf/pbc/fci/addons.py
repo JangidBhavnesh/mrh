a602485eea0d10b6473469ed5ac5022e27fa3d60
JangidBhavnesh <bhuvnesh.jangid10@gmail.com>
2026-07-25 15:30:05 -0500

    Added the spin-penalty for the kFCI solver

--- my_pyscf/pbc/fci/addons.py
+++ my_pyscf/pbc/fci/addons.py
@@ -4,9 +4,23 @@ from pyscf.fci import addons, cistring
 # I have added the contract_ss, so I hope I don't need to write the code for these
 # function, rather this should work.
 
-fix_spin = addons.fix_spin
-fix_spin_ = addons.fix_spin_
 SpinPenaltyFCISolver = addons.SpinPenaltyFCISolver
+
+def _is_kfci_solver(fciobj):
+    from mrh.my_pyscf.pbc.fci import direct_spin1_kfci
+    return isinstance(fciobj, direct_spin1_kfci.FCISolver)
+
+def fix_spin(fciobj, shift=.1, ss=None, **kwargs):
+    if _is_kfci_solver(fciobj):
+        from mrh.my_pyscf.pbc.fci import direct_spin1_kfci
+        return direct_spin1_kfci.fix_spin(fciobj, shift=shift, ss=ss, **kwargs)
+    return addons.fix_spin(fciobj, shift=shift, ss=ss, **kwargs)
+
+def fix_spin_(fciobj, shift=.1, ss=None, **kwargs):
+    if _is_kfci_solver(fciobj):
+        from mrh.my_pyscf.pbc.fci import direct_spin1_kfci
+        return direct_spin1_kfci.fix_spin_(fciobj, shift=shift, ss=ss, **kwargs)
+    return addons.fix_spin_(fciobj, shift=shift, ss=ss, **kwargs)
 
 # Helper function to unpack electrons and active space.
 def _unpack_nelec(nelec, spin=None):
