73e5193cac1a8255b346af915eb285cfccf681cb
JangidBhavnesh <bhuvnesh.jangid10@gmail.com>
2026-07-26 08:42:35 -0500

    Adding the unit-test for charged kcasci AP

--- tests/pbc/test_pbc_kcasci.py
+++ tests/pbc/test_pbc_kcasci.py
@@ -5,10 +5,40 @@ from pyscf.pbc import scf
 from pyscf.pbc import gto as pgto
 
 from mrh.my_pyscf.pbc import mcscf
+from mrh.my_pyscf.pbc.mcscf import kcasci
 from mrh.my_pyscf.pbc.fci import direct_spin1_cplx
 
 
 class KnownValues(unittest.TestCase):
+
+    def test_charged_active_nelecas(self):
+        self.assertEqual(
+            kcasci.charged_active_nelecas(2, 8, 2, 0, charge=1),
+            (8, 7))
+        self.assertEqual(
+            kcasci.charged_active_nelecas(2, 8, 2, 0, charge=-1),
+            (9, 8))
+        self.assertEqual(
+            kcasci.charged_active_nelecas(2, 8, (1, 1), 0,
+                                          charge=1, spin=-1),
+            (7, 8))
+
+    def test_charged_band_energies(self):
+        results = [
+            {'target_k': 0, 'charge': 1, 'nkpts': 2,
+             'e_tot': np.asarray([-1.0, -0.8])},
+            {'target_k': 1, 'charge': 1, 'nkpts': 2,
+             'e_tot': np.asarray([-0.9, -0.7])},
+        ]
+        kpts = np.asarray([[0.0, 0.0, 0.0], [0.5, 0.0, 0.0]])
+        bands = kcasci.charged_band_energies(
+            results, reference_energy=-1.2, root=1, kpts=kpts)
+
+        self.assertEqual([b['target_k'] for b in bands], [0, 1])
+        self.assertIn('hole_momentum', bands[0])
+        self.assertTrue(np.allclose(bands[1]['hole_momentum'], kpts[1]))
+        self.assertAlmostEqual(bands[0]['energy'], 0.8)
+        self.assertAlmostEqual(bands[1]['energy'], 1.0)
 
     def test_kcasci_target_k0_vs_casci(self):
         intraH = 0.74
@@ -40,6 +70,15 @@ class KnownValues(unittest.TestCase):
         kmf.kernel()
 
         mo_coeff = np.asarray(kmf.mo_coeff)
+
+        kmc_default = mcscf.KCASCI(kmf, 2, 2)
+        self.assertIsInstance(kmc_default, kcasci.PBCKCASCI)
+        self.assertNotIsInstance(kmc_default, kcasci.ChargedKCASCI)
+        self.assertEqual(kmc_default.target_k, 0)
+        kmc_charged = mcscf.KCASCI(kmf, 2, 2, charge=1)
+        self.assertIsInstance(kmc_charged, kcasci.ChargedKCASCI)
+        self.assertIsNone(kmc_charged.target_k)
+        self.assertFalse(hasattr(mcscf, 'ChargedKCASCI'))
 
         kmc_ref = mcscf.CASCI(kmf, 2, 2)
         kmc_ref.kmesh = kmesh
