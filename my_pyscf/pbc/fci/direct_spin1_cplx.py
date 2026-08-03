a7ef134d48a386c6aab227cd8bcd6a937ef1fb9c
JangidBhavnesh <bhuvnesh.jangid10@gmail.com>
2026-08-02 23:34:31 -0500

    Adding the wrapper for make_rdm2

--- my_pyscf/pbc/fci/direct_spin1_cplx.py
+++ my_pyscf/pbc/fci/direct_spin1_cplx.py
@@ -316,6 +316,14 @@ def make_rdm12(fcivec, norb, nelec, link_index=None, reorder=True):
     rdm2 = dm2aa + dm2bb + dm2ab + dm2ab.transpose(2, 3, 0, 1)
     return rdm1.conj().T, rdm2
 
+def make_rdm2(fcivec, norb, nelec, link_index=None, reorder=True):
+    make_rdm2.__doc__ = direct_spin1.make_rdm2.__doc__ + '''
+    Compute the spin-summed 2-RDM for a complex FCI vector using 
+    the backend C function.
+    '''
+    rdm2 = make_rdm12(fcivec, norb, nelec, link_index=link_index, reorder=reorder)[1]
+    return rdm2
+
 def make_rdm1s_py(fcivec, norb, nelec, link_index=None):
     '''
     Python implementation of spin-separated 1-RDMs for a complex FCI vector.
@@ -629,6 +637,9 @@ class FCISolver(direct_spin1.FCISolver):
     def make_rdm12(self, fcivec, norb, nelec, link_index=None, reorder=True):
         return make_rdm12(fcivec, norb, nelec, link_index, reorder)
 
+    def make_rdm2(self, fcivec, norb, nelec, link_index=None, reorder=True):
+        return make_rdm2(fcivec, norb, nelec, link_index, reorder)
+    
     def get_init_guess(self, norb, nelec, nroots, hdiag):
         return get_init_guess_cplx(norb, nelec, nroots, hdiag)
     
