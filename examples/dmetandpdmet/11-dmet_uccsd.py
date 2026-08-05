eb42d6b0bc8b87f94fc26bbc158d136d4b8445d6
JangidBhavnesh <bhuvnesh.jangid10@gmail.com>
2026-08-05 14:43:31 -0500

    Update the example

--- examples/dmetandpdmet/11-dmet_uccsd.py
+++ examples/dmetandpdmet/11-dmet_uccsd.py
@@ -8,7 +8,7 @@ Author: Bhavnesh Jangid
 Example of using DMET to run the unrestricted coupled cluster calculations on a molecule.
 Basically, we need to start with the ROHF/RHF type HF but later on if we want to run
 the UCC type wave functions, we need (or atleast it's better to use) UHF reference 
-wave function. I have added the option to get the UHF reference wave function from the DMET code.
+wave function. In this example, we will show how to get the UHF wave function from the ROHF wave function.
 '''
 
 np.set_printoptions(precision=4)
@@ -27,7 +27,11 @@ mf.kernel()
 
 # To get back the UHF wave function, turn on the uhf=True flag.
 dmet_mf, mydmet = runDMET(mf, lo_method='lowdin', 
-                          bath_tol=1e-6, atmlst=[0, ], density_fit=True, uhf=True)
+                          bath_tol=1e-6, atmlst=[0, ], density_fit=True)
+
+# Convert the DMET mean-field object to UHF mean-field object and run the UHF calculation.
+dmet_mf = dmet_mf.to_uhf()
+dmet_mf.kernel()
 
 # Using the dmet_mf to run the UCCSD or type of calculations.
 # Feel free to set the parameters of the UCCSD object as per your requirement.
@@ -37,4 +41,6 @@ mycc.kernel()
 # Solve the lambda equations to get the spin square value.
 s2 = mycc.spin_square()[0]
 
+print("---"*30)
+print("DMET RHF Energy:", dmet_mf.e_tot, " and DMET RHF Spin Square: ", dmet_mf.spin_square()[0])
 print("UCCSD Energy: ", mycc.e_tot, " and UCCSD Spin Square: ", s2)
