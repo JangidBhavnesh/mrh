8066655d1491306955a4522611f72f506fc65e8b
JangidBhavnesh <bhuvnesh.jangid10@gmail.com>
2026-08-10 18:21:58 -0500

    Initial framework added

--- my_pyscf/pbc/mcscf/klasscf.py
+++ my_pyscf/pbc/mcscf/klasscf.py
@@ -1,16 +1,78 @@
 #!/usr/bin/env python
 
+import numpy as np
 
+from mrh.my_pyscf.mcscf.lasscf_sync_o0 import (
+    LASSCF_HessianOperator as molLASSCF_HessianOperator,
+)
 
+class KLASSCF_HessianOperator(molLASSCF_HessianOperator):
+    """Periodic scaffold for the k-LASSCF Hessian operator.
 
+    Only the constructor is implemented for now.  In particular, the
+    molecular ``_matvec`` inherited from the parent class must not be used
+    until the periodic CI-hop methods have been supplied.
 
+    Parameters
+    ----------
+    las
+        Periodic LASCI/LASSCF object.
+    ugg
+        Unitary-group generator object used to pack and unpack trial vectors.
+    mo_coeff, ci
+        Current orbitals and CI vectors.  They default to the corresponding
+        attributes of ``las``.
+        mo_coeff : list or np.ndarray of shape (nkpts, nao, nmo) (in bloch representation)
+        ci : list or np.ndarray of shape (nroots, ncastot, ncastot) (in wannier basis)
+    kpts, kmesh
+        BvK k-points and k-point mesh.  They default to ``las.kpts`` and
+        ``las.kmesh`` and must describe the same number of cells.
 
+    Notes
+    -----
+    The molecular parent constructor is intentionally not called: it
+    immediately builds molecular orbital-response and ERI intermediates that
+    are not valid for the periodic CI-only operator.
+    """
 
+    def __init__(
+            self, las, ugg, mo_coeff=None, ci=None, casdm1frs=None,
+            h1eff=None, h2eff=None, kpts=None, kmesh=None):
 
+        if mo_coeff is None: mo_coeff = las.mo_coeff
+        if ci is None: ci = las.ci
+        if kpts is None: kpts = las.kpts
+        if kmesh is None: kmesh = las.kmesh
+        kpts = np.asarray(kpts)
+        kmesh = tuple(int(n) for n in kmesh)
+    
+        if len(kmesh) != 3 or any(n <= 0 for n in kmesh):
+            raise ValueError("kmesh must contain three positive integers")
+        ncell = int(np.prod(kmesh))
+        if len(kpts) != ncell:
+            raise ValueError(f"kpts and kmesh are inconsistent: {len(kpts)} != {ncell}")
+        
+        self.las = las
+        self.ugg = ugg
+        self.mo_coeff = mo_coeff
+        self.ci = ci
+        self.kpts = kpts
+        self.kmesh = kmesh
+        self.nkpts = len(kpts)
+        self.ncell = ncell
 
+        self.casdm1frs = casdm1frs
+        self.h1eff = h1eff
+        self.h2eff = h2eff
 
-
-# Author: Bhavnesh Jangid
-
-# Implementation of the k-LASSCF: The Hessian Operator:
-
+        self.level_shift = las.ah_level_shift
+        self.ncore = las.ncore
+        self.ncas_sub = np.asarray(las.ncas_sub)
+        self.nelecas_sub = np.asarray(las.nelecas_sub)
+        self.ncas = int(np.sum(self.ncas_sub))
+        self.nao = mo_coeff.shape[-2]
+        self.nmo = mo_coeff.shape[-1]
+        self.nocc = self.ncore + self.ncas
+        self.fciboxes = las.fciboxes
+        self.nroots = las.nroots
+        self.weights = las.weights
