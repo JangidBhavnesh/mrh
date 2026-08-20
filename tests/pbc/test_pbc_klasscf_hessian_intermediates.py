2679707070f060d90a56d69f7b72a279ad018616
JangidBhavnesh <bhuvnesh.jangid10@gmail.com>
2026-08-19 20:43:26 -0500

    Build k-LASSCF Hessian Hamiltonian intermediates

--- tests/pbc/test_pbc_klasscf_hessian_intermediates.py
+++ tests/pbc/test_pbc_klasscf_hessian_intermediates.py
@@ -13,9 +13,12 @@ class _IdentitySCF:
 
 
 class _DensityLAS:
-    def __init__(self, casdm2):
+    def __init__(self, casdm2, hcore=None):
         self._scf = _IdentitySCF()
         self.casdm2 = np.asarray(casdm2)
+        if hcore is None:
+            hcore = np.zeros((2, 3, 3))
+        self.hcore = np.asarray(hcore)
 
     @staticmethod
     def make_casdm1s_sub(casdm1frs=None):
@@ -31,6 +34,9 @@ class _DensityLAS:
     def make_casdm2(self, **kwargs):
         return np.array(self.casdm2, copy=True)
 
+    def get_hcore(self, kpts=None):
+        return np.array(self.hcore, copy=True)
+
 
 def make_operator(casdm2):
     operator = KLASSCF_HessianOperator.__new__(KLASSCF_HessianOperator)
@@ -39,6 +45,7 @@ def make_operator(casdm2):
     operator.ncas_sub = np.array([1, 1])
     operator.nelecas_sub = np.array([(1, 0), (1, 0)])
     operator.weights = np.array([1.0])
+    operator.nroots = 1
     operator.ncastot = 2
     operator.nkpts = 2
     operator.nao = 3
@@ -83,6 +90,33 @@ class KnownValues(unittest.TestCase):
         np.testing.assert_allclose(operator.cascm2, expected_cascm2)
         np.testing.assert_allclose(operator.dm1s_kpts, dm1s_kpts)
         np.testing.assert_allclose(operator.dm1s, dm1s_kpts)
+
+    def test_hamiltonian_intermediates(self):
+        hcore = np.array([
+            np.diag([1.0, 2.0, 3.0]),
+            np.diag([1.5, 2.5, 3.5]),
+        ])
+        veff_kpts = np.zeros((2, 2, 3, 3), dtype=np.complex128)
+        veff_kpts[0, 0] = np.diag([0.1, 0.2, 0.3])
+        veff_kpts[1, 0] = np.diag([0.4, 0.5, 0.6])
+        veff_kpts[0, 1] = np.diag([0.7, 0.8, 0.9])
+        veff_kpts[1, 1] = np.diag([1.0, 1.1, 1.2])
+        h2eff = np.arange(16, dtype=float).reshape((2,) * 4) / 17.0
+        h1eff = [
+            np.zeros((1, 2, 1, 1), dtype=np.complex128)
+            for _ in range(2)
+        ]
+        operator = make_operator(np.zeros((2,) * 4))
+        operator.las.hcore = hcore
+
+        operator._init_ham_(h1eff, h2eff, veff_kpts=veff_kpts)
+
+        np.testing.assert_allclose(operator.hcore, hcore)
+        np.testing.assert_allclose(
+            operator.h1s, hcore[None, :, :, :] + veff_kpts,
+        )
+        np.testing.assert_allclose(operator.eri_cas, h2eff)
+        self.assertIs(operator.h1frs, h1eff)
 
     def test_rejects_inconsistent_active_density_shape(self):
         operator = make_operator(np.zeros((2,) * 4))
