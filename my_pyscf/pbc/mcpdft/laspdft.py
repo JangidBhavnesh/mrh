d52fb75aab8180f68d58e70928d1728b3da80c22
JangidBhavnesh <bhuvnesh.jangid10@gmail.com>
2026-08-20 20:23:22 -0500

    Cleanups

--- my_pyscf/pbc/mcpdft/laspdft.py
+++ my_pyscf/pbc/mcpdft/laspdft.py
@@ -1,20 +1,26 @@
-"""LAS-PDFT support for periodic gamma-point calculations."""
-
 from mrh.my_pyscf.mcpdft import laspdft as molecular_laspdft
 from mrh.my_pyscf.pbc.mcpdft.mcpdft import _MCPDFT
 
+# Author: Bhavnesh Jangid
+
+"""
+LAS-PDFT support for periodic gamma-point calculations.
+"""
 
 class _LASPDFT(_MCPDFT, molecular_laspdft._LASPDFT):
-    """Periodic MC-PDFT energy for a localized active-space wavefunction."""
-
+    """
+    Periodic MC-PDFT energy for a localized active-space wavefunction.
+    """
     @property
     def cell(self):
         return self._scf.cell
 
 
 def get_mcpdft_child_class(las, ot, DoLASSI=False, states=None, **kwargs):
-    """Combine a gamma-point LAS solver with periodic LAS-PDFT methods."""
-    return molecular_laspdft.get_mcpdft_child_class(
+    """
+    Combine a gamma-point LAS solver with periodic LAS-PDFT methods.
+    """
+    laspdft = molecular_laspdft.get_mcpdft_child_class(
         las,
         ot,
         DoLASSI=DoLASSI,
@@ -22,3 +28,4 @@ def get_mcpdft_child_class(las, ot, DoLASSI=False, states=None, **kwargs):
         _pdft_class=_LASPDFT,
         **kwargs,
     )
+    return laspdft
