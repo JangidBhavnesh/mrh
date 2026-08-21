85b97b5960ebfb931788c277538174e15e63ec1b
JangidBhavnesh <bhuvnesh.jangid10@gmail.com>
2026-08-20 20:48:20 -0500

    Cleanup

--- my_pyscf/mcpdft/otfnalperiodic.py
+++ my_pyscf/mcpdft/otfnalperiodic.py
@@ -1,8 +1,4 @@
-"""Backward-compatible imports for periodic on-top functionals.
-
-Periodic MC-PDFT is implemented in :mod:`mrh.my_pyscf.pbc.mcpdft`.  This
-module retains the historical import paths used by downstream callers.
-"""
+# !/usr/bin/env python
 
 from mrh.my_pyscf.pbc.mcpdft.otfnalperiodic import (
     _get_ks_obj,
@@ -16,6 +12,14 @@ from mrh.my_pyscf.pbc.mcpdft.otfnalperiodic import (
     sanity_check_for_kpts,
 )
 
+# Author: Bhavnesh Jangid
+
+"""
+Backward-compatible imports for periodic on-top functionals.
+
+Periodic MC-PDFT is implemented in :mod:`mrh.my_pyscf.pbc.mcpdft`.  This
+module retains the historical import paths used by downstream callers.
+"""
 
 otfnalperiodic = otfnalperiodic_gamma
 _get_transfnal = get_pbc_otfnal_gamma
