ac5977156a7978a81416affab54e346955517a76
JangidBhavnesh <bhuvnesh.jangid10@gmail.com>
2026-08-23 08:21:18 -0500

    remove the small fn

--- my_pyscf/pbc/fci/addons.py
+++ my_pyscf/pbc/fci/addons.py
@@ -3,26 +3,24 @@ from pyscf.fci import addons, cistring
 
 from mrh.my_pyscf.pbc.fci import direct_spin1_kfci
 
+# Author: Bhavnesh Jangid
+
 """
 Periodic FCI add-ons and compatibility dispatch.
 """
 
 SpinPenaltyFCISolver = addons.SpinPenaltyFCISolver
 
-def _is_kfci_solver(fciobj):
-    """Return whether an object uses the momentum-sector FCI solver."""
-    return isinstance(fciobj, direct_spin1_kfci.FCISolver)
-
 def fix_spin(fciobj, shift=0.1, ss=None, **kwargs):
     """Dispatch spin-penalty construction to the matching solver family."""
-    if _is_kfci_solver(fciobj):
+    if isinstance(fciobj, direct_spin1_kfci.FCISolver):
         return direct_spin1_kfci.fix_spin(fciobj, shift=shift, 
                                           ss=ss, **kwargs)
     return addons.fix_spin(fciobj, shift=shift, ss=ss, **kwargs)
 
 def fix_spin_(fciobj, shift=0.1, ss=None, **kwargs):
     """Apply the appropriate spin-penalty mixin in place."""
-    if _is_kfci_solver(fciobj):
+    if isinstance(fciobj, direct_spin1_kfci.FCISolver):
         return direct_spin1_kfci.fix_spin_(fciobj, shift=shift,
                                            ss=ss, **kwargs)
     return addons.fix_spin_(fciobj, shift=shift, ss=ss, **kwargs)
